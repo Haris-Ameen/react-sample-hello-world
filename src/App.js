e2a3746f4fb20968192fecad1b33b6102e69cea8
Haris Amin <57298919+CEOHarisAmin@users.noreply.github.com>
2020-06-08 20:10:48 +0500

    react sample hello world

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World From Haris Amin <code></code> 
         </p>
+        <a href="">17cs28@quest.edu.pk</a>
         <a
           className="App-link"
           href="https://reactjs.org"
